473e4313f0246c87d671800dc163905c935fed45
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-01-03 21:22:02 +0100

    reformat overflow code

--- demo/simple_overflow_cli/overflow.c
+++ demo/simple_overflow_cli/overflow.c
@@ -1,27 +1,33 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
-void secret_function() {
-  printf("\nYou have reached the secret code!\n");
+void str_to_lowercase(char *str);
+void vulnerable_code(char *input_str);
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    perror("usage: <program> <input>");
+    return 1;
+  }
+
+  vulnerable_code(argv[1]);
 }
 
-void str_to_lowercase(char* buf){
-  int length = strlen(buf);
+void vulnerable_code(char *input_str) {
+  char small_buffer[32];
+  strcpy(small_buffer, input_str);
+  str_to_lowercase(small_buffer);
+  printf("The string '%s' in lowercase is: '%s'\n", input_str, small_buffer);
+}
+
+void str_to_lowercase(char *str) {
+  int length = strlen(str);
   for (int i = 0; i < length; ++i) {
-    buf[i] = tolower(buf[i]);
+    str[i] = tolower(str[i]);
   }
 }
 
-void vulnerable_code(char * input_buffer){
-  char small_buffer[32];
-  strcpy(small_buffer, input_buffer);
-  str_to_lowercase(small_buffer);
-  printf("The string '%s' in lowercase is: '%s'\n",input_buffer,small_buffer);
+void secret_function() {
+  printf("\n!!!You have reached the secret code!!!\n");
 }
-
-int main(int argc, char *argv[]) {
-  vulnerable_code(argv[1]);
-  return 0;
-}
-
