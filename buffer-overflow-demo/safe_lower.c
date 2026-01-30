4a2518f70753ea32b0a56d5be6a1203dfe5e5c1e
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-01-30 23:55:12 +0100

    reformat

--- buffer-overflow-demo/safe_lower.c
+++ buffer-overflow-demo/safe_lower.c
@@ -5,8 +5,9 @@
 
 void str_to_lowercase(char *str) {
     int length = strlen(str);
-    for (int i = 0; i < length; ++i)
+    for (int i = 0; i < length; ++i) {
         str[i] = tolower(str[i]);
+    }
 }
 
 void lowercase_print(char *input_str) {
@@ -31,5 +32,6 @@ int main(int argc, char *argv[]) {
         perror("usage: <program> <input>\n");
         return 1;
     }
+
     lowercase_print(argv[1]);
 }
